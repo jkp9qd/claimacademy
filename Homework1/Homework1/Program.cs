0303ce340f2dc1bddfeb6289f105f4efd8a6ab9e
Jarvis Potter <jariviskpotter@gmail.com>
2024-08-19 19:05:21 -0500

    submit homework1

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string aboutme = ("Hi, My name is Jarvis Potter, I plan to be a successful Software Engineer!");
+            Console.WriteLine(aboutme);
+
+            int product = Multiply(4, 8);
+            Console.WriteLine(Multiply);
+            Console.WriteLine($"The product is:(product)");
+            
+            for (int i = 0; i < args.Length; i++) 
+            {
+
+
+        }
+    }
+}
