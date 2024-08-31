b5f963f52ef689b1436cce490c124b30c936ef66
Jarvis Potter <jarviskpotter@gmail.com>
2024-08-31 11:33:03 -0500

    Second Commit

--- CashRegister/CashRegister/Program.cs
+++ CashRegister/CashRegister/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace CashRegister
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        protected string item_name;
+        protected double item_price;
+        protected int item_quantiy;
+        protected double total_price;
+
+        public Program(string name, double price, int quantity)
         {
+            this.item_name = name;
+            this.item_price = price;
+            this.item_quantiy = quantity;
+            this.total_price = price + quantity;
         }
+        static void Main(string, double, int quantity)[] args){
+            }
+
+        public double getTotalPrice()
+        {
+
+            return total_price;
+        }
+
     }
 }
